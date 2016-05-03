83bd58e08ced87c3e0d7e74b06bf0b7f6db50c4d
Jeff Gulbronson <jgulbronson@gmail.com>
2016-05-02 21:05:28 -0400

    Allowed PUT requests

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require('express');
-
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+var redis = require('redis');
+var client = redis.createClient();
+var bodyParser = require('body-parser')
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'app')));
 
 app.get('/', function(req, res, next) {
   res.status(200).sendFile(path.join(__dirname + '/app/index.html'));
+});
+
+app.put('/api/v1/queue', function(req, res) {
+  console.log(req.body);
+  res.sendStatus(200);
 });
 
 http.listen(port, function() {
